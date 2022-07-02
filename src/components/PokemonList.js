e9a1847bd79c210938dbedf8a1ab078558865450
Geoph88 <geoffrey_sizgoric@hotmail.com>
2022-07-02 12:21:17 +1000

    added search functionality for specific pokemon. Bug with menu dropdown bars when searching for a type

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -14,18 +14,38 @@ import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import CardHeader from '@mui/material/CardHeader';
+import SearchPokemon from './SearchPokemon'
+import InputLabel from '@mui/material/InputLabel';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+
 
 function PokemonList() {
   const [PokemonList, setPokemonList] = useState([])
+  const [searchBar, setSearchBar] = useState(null)
   const [page, setPage] = useState(1);
   const [offset, setOffset] = useState(0)
-  const [favouritePokemon, setFavouritePokemon] = useState({
+  const [favouritePokemon, setFavouritePokemon] = useState
+  ({
     pokemonName: [],
     pokedex_id: [],
     image: []
   })
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
-  
+  const handleClick = (event) => {
+    console.log(event)
+    setAnchorEl(event.currentTarget);
+  };
+
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   useEffect(() => {
     const getPokemonData = async() => {
@@ -34,10 +54,10 @@ function PokemonList() {
     const endOffset = offset + 20
     const currentPokemonList = response.results.slice(offset, endOffset)
     setPokemonList(currentPokemonList)
+    setSearchBar(response.results)
   }
     getPokemonData()
-  })
-
+  }, [])
   
   function saveFavouritePokemon (pokemon, id, image) {
     if (favouritePokemon !== null) {
@@ -51,30 +71,77 @@ function PokemonList() {
     }
   }
 
-  // useEffect(saveFavouritePokemon, [favouritePokemon])
-
   const handleChange = (event, value) => {
     setPage(value);
     const newOffset = pokemonPages[value]
     setOffset(newOffset - 20)
   };
 
-  
-  const style = {
-    boxShadow: '0 1 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24) !important'
-  }
-
+  const types = ['Bug',	'Dark',	'Dragon',	'Electric',	'Fighting',	'Fighting',	'Fire',	'Flying',	'Ghost',
+  'Grass',	'Ground',	'Ice',	'Normal',	'Poison',	'Psychic',	'Rock',	'Steel',	'Water']
   
 
 
   return (  
   <>
     <h1>Welcome to the pokedex</h1>
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Search for a Pokemon
+      </Button>
+      
+      <Menu
+        id="basic-menu-two"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-two',
+        }}
+      >
+        {searchBar && searchBar.map((pokemon, index) =>
+        <Link to={`/PokemonDetails/${pokemon.name}`}>
+        <MenuItem key={index}>{pokemon.name}</MenuItem>
+        </Link>
+        )}
+      </Menu>
+    </div>
+
+    <div>
+    <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        Search for a Type
+      </Button>
+       <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        {types.map((type, index) =>
+        <MenuItem key={index}>{type}</MenuItem>
+        )}
+      </Menu> 
+      </div>
+
       <section className="pokemon-list-container">
-        
         {PokemonList.map((pokemon, index) =>
         <Card sx={{ maxWidth: 345, margin: '2rem' }}>
-        <div className='pokemon-container' style={style} key={index}>
+        <div className='pokemon-container' key={index}>
           <Typography gutterBottom variant="h5" component="div" className='pokedex_id'>
               {pokemon.url.split('/')[pokemon.url.split('/').length - 2]}
             </Typography>
---
+++ src/components/SearchPokemon.js
@@ -0,0 +1,11 @@
+import PokemonList from "./PokemonList";
+
+export default function SearchPokemon({searchBar}) {
+
+
+  return (
+    <>
+    
+  </>
+  )
+}
